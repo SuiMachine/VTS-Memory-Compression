f115debba67d2d209a0b117d8b8a7bd82f7f0be5
Sui / SuicideMachine <SuiMachine@users.noreply.github.com>
2025-12-28 16:45:44 +0100

    Moved to NetStandard 2.1 to allow for linking some libraries you normally can't with Framework

--- Plugin.cs
+++ Plugin.cs
@@ -1,30 +1,32 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using BepInEx.Unity.Mono;
+using HarmonyLib;
 
-namespace VTSMemoryCompression;
-
-[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-public class Plugin : BaseUnityPlugin
+namespace VTSMemoryCompression
 {
-	private static BepInEx.Logging.ManualLogSource LoggerInstance;
-	private static HarmonyLib.Harmony HarmonyInstance;
-
-	private void Awake()
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
     {
-		// Plugin startup logic
-		LoggerInstance = base.Logger;
-		HarmonyInstance = new HarmonyLib.Harmony("local.VTSMemoryCompression.SuiMachine");
-		Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+		public static ManualLogSource LoggerInstance { get; private set; }
+		public static Harmony HarmonyInstance { get; private set; }
 
-		Logger.LogInfo($"Patching...");
-		HarmonyInstance.PatchAll();
-		Logger.LogInfo($"Patched!");
+		private void Awake()
+        {
+			// Plugin startup logic
+			LoggerInstance = base.Logger;
+			HarmonyInstance = new HarmonyLib.Harmony("local.VTSMemoryCompression.SuiMachine");
+			Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+			Logger.LogInfo($"Patching...");
+			HarmonyInstance.PatchAll();
+			Logger.LogInfo($"Patched!");
+		}
+
+		public static void LogMessage(string text) => LoggerInstance.LogMessage(text);
+
+		public static void LogError(string text) => LoggerInstance.LogError(text);
+
+		public static void LogWarning(string text) => LoggerInstance.LogWarning(text);
 	}
-
-	public static void LogMessage(string text) => LoggerInstance.LogMessage(text);
-
-	public static void LogError(string text) => LoggerInstance.LogError(text);
-
-	public static void LogWarning(string text) => LoggerInstance.LogWarning(text);
 }
