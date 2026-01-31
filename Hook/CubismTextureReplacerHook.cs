aa98477d2260c3a61e1c10673a6467d5a66afa03
Sui / SuicideMachine <SuiMachine@users.noreply.github.com>
2026-01-31 22:19:00 +0100

    Cleaner code for benchmark

--- Hook/CubismTextureReplacerHook.cs
+++ Hook/CubismTextureReplacerHook.cs
@@ -1,5 +1,8 @@
-﻿using HarmonyLib;
+﻿//#define BENCHMARK
+using HarmonyLib;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using UnityEngine;
 
@@ -9,52 +12,8 @@ namespace VTSMemoryCompression.Hook
 	public static class CubismTextureReplacerHook
 	{
 #if BENCHMARK
-		public static Dictionary<int, List<TimeSpan>> BenchmarkValues = new Dictionary<int, List<TimeSpan>>();
+		public static Dictionary<string, List<TimeSpan>> BenchmarkValues = new Dictionary<string, List<TimeSpan>>();
 
-		//[HarmonyPrefix]
-		//[HarmonyPatch(typeof(IOHelper), "CubismLoadAssetFromStreamingAssets")]
-		public static bool CubismLoadAssetFromStreamingAssetsDetourOG(ref object __result, Type assetType, string absolutePath)
-		{
-			//This is for benchmark purposes - could be reworked to offer optional DXT5 runtime compression, though
-			if (assetType != typeof(Texture2D))
-				return true;
-
-			Stopwatch sw = Stopwatch.StartNew();
-			Texture2D newTexture = new Texture2D(4, 4, TextureFormat.RGBA32, true);
-			newTexture.wrapMode = TextureWrapMode.Clamp;
-
-			var bytesLoaded = File.ReadAllBytes(absolutePath);
-			newTexture.LoadImage(bytesLoaded);
-			//newTexture.Compress(true);
-			newTexture.Apply(false, true);
-			sw.Stop();
-
-			if (BenchmarkValues.TryGetValue(newTexture.width, out List<TimeSpan> foundBenchmarkValues))
-			{
-				foundBenchmarkValues.Add(sw.Elapsed);
-			}
-			else
-			{
-				foundBenchmarkValues = new List<TimeSpan>()
-				{
-					sw.Elapsed
-				};
-				BenchmarkValues.Add(newTexture.width, foundBenchmarkValues);
-			}
-
-			TimeSpan average = TimeSpan.Zero;
-			foreach (TimeSpan element in foundBenchmarkValues)
-				average += element;
-			average /= foundBenchmarkValues.Count;
-
-			Plugin.LogMessage($"LOAD BENCHMARK {newTexture.width} ({foundBenchmarkValues.Count} - {newTexture.format}) = {average}");
-
-			__result = newTexture;
-
-			return false;
-		}
-
-		
 		[HarmonyPrefix]
 		[HarmonyPatch(typeof(IOHelper), "CubismLoadAssetFromStreamingAssets")]
 		public static bool CubismLoadAssetFromStreamingAssetsDetour(ref object __result, Type assetType, string absolutePath)
@@ -76,7 +35,7 @@ namespace VTSMemoryCompression.Hook
 				if (createdTexture != null)
 				{
 					sw.Start();
-					if (BenchmarkValues.TryGetValue(createdTexture.width, out List<TimeSpan> foundBenchmarkValues))
+					if (BenchmarkValues.TryGetValue(absolutePath, out List<TimeSpan> foundBenchmarkValues))
 					{
 						foundBenchmarkValues.Add(sw.Elapsed);
 					}
@@ -86,14 +45,14 @@ namespace VTSMemoryCompression.Hook
 						{
 							sw.Elapsed
 						};
-						BenchmarkValues.Add(createdTexture.width, foundBenchmarkValues);
+						BenchmarkValues.Add(absolutePath, foundBenchmarkValues);
 					}
 
 					TimeSpan average = TimeSpan.Zero;
 					foreach (TimeSpan element in foundBenchmarkValues)
 						average += element;
 					average /= foundBenchmarkValues.Count;
-					Plugin.LogMessage($"LOAD BENCHMARK {createdTexture.width} ({foundBenchmarkValues.Count} - {createdTexture.format}) = {average}");
+					Plugin.LogMessage($"LOAD BENCHMARK (DDS) {absolutePath} ({foundBenchmarkValues.Count} - {createdTexture.format}) = {average}");
 
 					__result = createdTexture;
 					GC.Collect();
@@ -102,8 +61,42 @@ namespace VTSMemoryCompression.Hook
 				else
 					return true;
 			}
+			else
+			{
+				//This is for benchmark purposes - could be reworked to offer optional DXT5 runtime compression, though
+				Stopwatch sw = Stopwatch.StartNew();
+				Texture2D newTexture = new Texture2D(4, 4, TextureFormat.RGBA32, true);
+				newTexture.wrapMode = TextureWrapMode.Clamp;
 
-			return true;
+				var bytesLoaded = File.ReadAllBytes(absolutePath);
+				newTexture.LoadImage(bytesLoaded);
+				//newTexture.Compress(true);
+				newTexture.Apply(false, true);
+				sw.Stop();
+
+				if (BenchmarkValues.TryGetValue(absolutePath, out List<TimeSpan> foundBenchmarkValues))
+				{
+					foundBenchmarkValues.Add(sw.Elapsed);
+				}
+				else
+				{
+					foundBenchmarkValues = new List<TimeSpan>()
+					{
+						sw.Elapsed
+					};
+					BenchmarkValues.Add(absolutePath, foundBenchmarkValues);
+				}
+
+				TimeSpan average = TimeSpan.Zero;
+				foreach (TimeSpan element in foundBenchmarkValues)
+					average += element;
+				average /= foundBenchmarkValues.Count;
+
+				Plugin.LogMessage($"LOAD BENCHMARK (RAW) {absolutePath} ({foundBenchmarkValues.Count} - {newTexture.format}) = {average}");
+
+				__result = newTexture;
+				return false;
+			}
 		}
 #else
 		[HarmonyPrefix]
